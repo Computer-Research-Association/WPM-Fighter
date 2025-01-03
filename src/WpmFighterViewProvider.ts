dd551d3deeedec204a0200c27746c06ff9f88562
Jangmyun <cndlsrb2739@gmail.com>
2025-01-04 02:00:43 +0900

    [feat] webviewViewProvider 멤버변수 선언

--- src/WpmFighterViewProvider.ts
+++ src/WpmFighterViewProvider.ts
@@ -1,6 +1,16 @@
 import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
 
-class WpmFighterViewProvider implements vscode.WebviewViewProvider {
+export default class WpmFighterViewProvider implements vscode.WebviewViewProvider {
+  public static readonly viewID = "wpmView";
+  private _webviewView?: vscode.WebviewView;
+  private _context: vscode.ExtensionContext;
+
+  constructor(context: vscode.ExtensionContext) {
+    this._context = context;
+  }
+
   public resolveWebviewView(
     webviewView: vscode.WebviewView,
     context: vscode.WebviewViewResolveContext,
