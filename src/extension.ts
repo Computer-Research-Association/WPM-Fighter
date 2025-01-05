4ff8b81f58b5e624ecbe265fbc92544c15aebfa3
Jangmyun <cndlsrb2739@gmail.com>
2025-01-05 12:15:30 +0900

    [refactor] webviewID를 webviewViewProvider 클래스 변수로 가져오기

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import WpmFighterViewProvider from "./WpmFighterViewProvider";
 export function activate(context: vscode.ExtensionContext) {
   const wpmFighterProvider = new WpmFighterViewProvider(context);
 
-  const webView = vscode.window.registerWebviewViewProvider("wpmView", wpmFighterProvider);
+  const webView = vscode.window.registerWebviewViewProvider(WpmFighterViewProvider.viewID, wpmFighterProvider);
   context.subscriptions.push(webView);
 }
 
