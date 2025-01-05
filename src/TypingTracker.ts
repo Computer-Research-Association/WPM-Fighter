ca6ef1d72b2ec1395afb51f53745f5047a14a199
Jangmyun <cndlsrb2739@gmail.com>
2025-01-05 12:46:28 +0900

    [feat #1] singleton 패턴의 TypingTracker 선언

--- src/TypingTracker.ts
+++ src/TypingTracker.ts
@@ -1,1 +1,20 @@
 import * as vscode from "vscode";
+
+class TypingTracker {
+  private static instance: TypingTracker;
+
+  private charCount: number = 0;
+  private startTime: number = Date.now();
+  private wpm: number = 0;
+  private timer?: NodeJS.Timeout;
+  private isTracking: boolean = false;
+
+  private constructor() {}
+
+  public static getInstance(): TypingTracker {
+    if (!TypingTracker.instance) {
+      TypingTracker.instance = new TypingTracker();
+    }
+    return TypingTracker.instance;
+  }
+}
