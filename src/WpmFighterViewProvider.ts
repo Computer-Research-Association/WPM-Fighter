0123564607633a83257ed819cedff7bbefbd289c
Jangmyun <cndlsrb2739@gmail.com>
2025-01-05 12:16:28 +0900

    [remove] remove unused module

--- src/WpmFighterViewProvider.ts
+++ src/WpmFighterViewProvider.ts
@@ -1,6 +1,4 @@
 import * as vscode from "vscode";
-import * as fs from "fs";
-import * as path from "path";
 
 export default class WpmFighterViewProvider implements vscode.WebviewViewProvider {
   public static readonly viewID = "wpmView";
