9b094c93227d3f77f61f56e15bd484a15291f6a4
Jangmyun <cndlsrb2739@gmail.com>
2025-01-05 15:05:18 +0900

    [feat #1] TypeTracker가 ExtensionContext를 소유함.

--- src/TypingTracker.ts
+++ src/TypingTracker.ts
@@ -3,17 +3,20 @@ import * as vscode from "vscode";
 class TypingTracker {
   private static instance: TypingTracker;
 
+  private _context: vscode.ExtensionContext;
   private charCount: number = 0;
   private startTime: number = Date.now();
   private wpm: number = 0;
   private timer?: NodeJS.Timeout;
   private isTracking: boolean = false;
 
-  private constructor() {}
+  private constructor(context: vscode.ExtensionContext) {
+    this._context = context;
+  }
 
-  public static getInstance(): TypingTracker {
+  public static getInstance(context: vscode.ExtensionContext): TypingTracker {
     if (!TypingTracker.instance) {
-      TypingTracker.instance = new TypingTracker();
+      TypingTracker.instance = new TypingTracker(context);
     }
     return TypingTracker.instance;
   }
